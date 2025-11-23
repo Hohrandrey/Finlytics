27fdb347f5db5ac0798d82b85a1b16921ebf722c
n1kpavlov <nikniknik388@gmail.com>
2025-11-23 15:09:26 +0300

    Add models

---
+++ src/main/kotlin/models/Operation.kt
@@ -0,0 +1,12 @@
+package models
+
+import java.time.LocalDate
+
+data class Operation(
+    val id: Int,
+    val type: String,
+    val amount: Double,
+    val category: String,
+    val date: LocalDate,
+    val name: String? = null
+)
