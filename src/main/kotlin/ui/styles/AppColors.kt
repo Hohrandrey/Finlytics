5170c18e916f565647a7426651e4e567dfe7f908
n1kpavlov <nikniknik388@gmail.com>
2025-12-12 16:55:09 +0300

    Add AppColors

---
+++ src/main/kotlin/ui/styles/AppColors.kt
@@ -0,0 +1,18 @@
+package main.ui.styles
+
+import androidx.compose.ui.graphics.Color
+
+object AppColors {
+    val DarkColor = Color(0xFF1E1E1E)
+    val DarkGreyColor = Color(0xFF2C2C2C)
+    val LightGreyColor = Color(0xFF444444)
+    val LightColor = Color(0xFFF4F4F4)
+    val BlueColor = Color(0xFF2176FF)
+    val RedColor = Color(0xFFEF4444)
+    val GreenColor = Color(0xFF4ADE80)
+    val YellowColor = Color(0xFFECB324)
+    val PurpleColor = Color(0xFF7300FF)
+    val RoseColor = Color(0xFFFF00A6)
+    val EmeraldColor = Color(0xFF00D9FB)
+    val LimeColor = Color(0xFFEAFF00)
+}
